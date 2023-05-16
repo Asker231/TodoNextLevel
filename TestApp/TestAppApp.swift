0dd76570589a1f419dc09cb80db9953382f4565c
Asker <kotsevasker0@gmail.com>
2023-05-16 07:33:40 +0300

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by ASKER on 16.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
